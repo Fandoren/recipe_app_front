e8828d0c8e56e7f322f1505ee8606b51d237d9fd
Fandoren <aleks.fandoren@gmail.com>
2023-09-08 14:20:45 +0300

    test photo loading

--- src/components/tags/TagCreatePage.jsx
+++ src/components/tags/TagCreatePage.jsx
@@ -5,9 +5,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleLeft } from '@fortawesome/free-regular-svg-icons';
 import ImageInputPreview from '../util/ImageInputPreview';
 
+import axios from "axios";
+
 function TagCreatePage() {
 
     const [images, setImages] = useState([]);
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
 
     function handleImagesChange(e) {
         setImages(e.target.files);
@@ -15,6 +19,25 @@ function TagCreatePage() {
 
     function onBackButtonClick(e) {
         console.log("Возврат назад");
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        var formData = new FormData();
+        for (let i = 0; i < images.length; i++) {
+            formData.append("images", images[i])
+        }
+        formData.append("name", name);
+        formData.append("description", description);
+        console.log(formData);
+        sendTag(formData);
+    }
+
+    async function sendTag(tag) {
+        const response = await axios.post("http://localhost:8080/test", tag, {
+            headers: { "Content-Type": "multipart/form-data" }
+        });
+        return response;
     }
 
     return(
@@ -29,7 +52,7 @@ function TagCreatePage() {
                 <Container className='create-container'>
                     <Row className='justify-content-center'>
                         <Col lg={11}>
-                            <Form className='create-form'>
+                            <Form className='create-form' onSubmit={handleSubmit}>
                                 <Form.Group className="mb-3 name" controlId="tagImages">
                                     <Form.Label>Изображения для тега</Form.Label>
                                     <Form.Control 
@@ -46,11 +69,21 @@ function TagCreatePage() {
                                 </Form.Group>
                                 <Form.Group className="mb-3 name" controlId="tagName">
                                     <Form.Label>Наименование тега</Form.Label>
-                                    <Form.Control type="text" placeholder="Введите наименование тега" />
+                                    <Form.Control 
+                                        value={name}
+                                        onChange={e => setName(e.target.value)}
+                                        type="text" 
+                                        placeholder="Введите наименование тега" 
+                                    />
                                 </Form.Group>
                                 <Form.Group className="mb-3 description" controlId="tagDescription">
                                     <Form.Label>Описание тега</Form.Label>
-                                    <Form.Control as="textarea" rows={3} />
+                                    <Form.Control
+                                        value={description}
+                                        onChange={e => setDescription(e.target.value)} 
+                                        as="textarea" 
+                                        rows={3} 
+                                    />
                                 </Form.Group>
                                 <Row className='flex-row-reverse'>
                                     <Col className='p-0' lg={2}>
