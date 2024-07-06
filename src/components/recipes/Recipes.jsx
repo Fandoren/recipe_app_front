9aed2a832f75805ee995b497aa30cc072b95c787
Aleksei Surmin <aleks.fandoren@gmail.com>
2024-07-06 13:59:35 +0300

    https://tracker.yandex.ru/RECIPEFRONT-10

--- src/components/recipes/Recipes.jsx
+++ src/components/recipes/Recipes.jsx
@@ -1,6 +1,21 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import RecipeCard from "./RecipeCard/RecipeCard";
 
 export default function Recipes() {
-  return <Container></Container>;
+  return (
+    <Container className="mt-5">
+      <Row className="justify-content-center">
+        <Col md={3}>
+          <RecipeCard />
+        </Col>
+        <Col md={3}>
+          <RecipeCard />
+        </Col>
+        <Col md={3}>
+          <RecipeCard />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
